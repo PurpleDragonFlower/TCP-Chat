b9b91b3544a668349b52480c0b66a9f616c939c1
millyxun <xunmilly@gmail.com>
2016-09-17 16:15:56 -0700

    Implemented message box and TCP connection
    Added message box to print responses from the server.
    Connected setup TCP connection in Tkinter class to send data to the
    server.

--- client.py
+++ client.py
@@ -1,46 +1,62 @@
 #documentation for ThreadedSocketServer is from https://docs.python.org/3.5/library/socketserver.html
-#will implement Tkinter later
+#Reference to http://effbot.org/tkinterbook/tkinter-index.htm for Tkinter
+
 import socket
-import sys
-#from tkinter import *
+#import sys #will implement parsing args later
 import tkinter as tk
 
-class chatGUI(tk.Tk):
-	def __init__(self):
-		tk.Tk.__init__(self)
-		self.title("Chat Room")
-		self.make_widgets()
-		
-	def make_widgets(self):
-		self.label = tk.Label(self, text="Hello, welcome to Milly's Chatroom (tentative)")
-		self.label.pack(fill='x')
-		var = tk.StringVar()
-		self.entry = tk.Entry(self, textvariable=var)
-		self.entry.pack(fill='x')
-		self.bt = tk.Button(self, text="Submit", command=sendData)
-		self.bt.pack()
-		
-	#put socket in here
-	def sendData:
-		pass
-		
-	def exitProgram():
-		self.quit()
-
 HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
-
-    # Receive data from the server and shut down
-    received = str(sock.recv(1024), "utf-8")
+class chatGUI(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("Chat Room")
+        self.make_widgets()
+        
+    def make_widgets(self):
+        # Introduction label
+        self.label = tk.Label(self, text="Hello, welcome to Milly's Chatroom (tentative)")
+        self.label.pack(fill='x')
+        
+        # Holds listbox and scrollbar of message window
+        self.message = tk.PanedWindow()
+        self.message.pack(fill=tk.BOTH)
+        
+        scrollbar = tk.Scrollbar(self.message)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        
+        self.messagebox = tk.Listbox(self.message, yscrollcommand=scrollbar.set)
+        self.messagebox.pack(fill=tk.BOTH)
+        
+        scrollbar.config(command=self.messagebox.yview)
+        
+        # Textbox of data to send
+        var = tk.StringVar()
+        self.entry = tk.Entry(self, textvariable=var)
+        self.entry.pack(fill=tk.X)
+        
+        # Sends data from the entry to the server
+        self.bt = tk.Button(self, text="Submit", command=self.sendData)
+        self.bt.pack()
+        
+    def sendData(self):
+        # Create a TCP socket
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            #get data from entry
+            data = self.entry.get()
+            
+            # Connect to server and send data
+            sock.connect((HOST, PORT))
+            sock.sendall(bytes(data + "\n", "utf-8"))
+            
+            # Receive data from the server and shut down
+            received = str(sock.recv(1024), "utf-8")
+            
+            # Writes back response to the message box
+            self.messagebox.insert(tk.END, received)
+        
+    def exitProgram():
+        self.quit()
 
 chat = chatGUI()
 chat.mainloop()
-	
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
